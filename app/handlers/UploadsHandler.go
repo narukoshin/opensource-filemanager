adfe51b624b04aadf64f2d661c3a9bdd9a173439
Naru Koshin <naru.koshin@outlook.jp>
2023-02-03 04:36:09 +0200

    improved file upload functionality.

--- app/handlers/UploadsHandler.go
+++ app/handlers/UploadsHandler.go
@@ -4,8 +4,10 @@ import (
 	"filemanager/app/sessions"
 	"filemanager/app/templates"
 	"fmt"
-	"io/ioutil"
+	"io"
+
 	"net/http"
+	"os"
 	"path/filepath"
 )
 
@@ -24,27 +26,32 @@ func FileUploadHandler(w http.ResponseWriter, r *http.Request){
 	if err != nil {
 		panic(err)
 	}
-
 	// throwing session handler to the black hole
 	current_directory, err := s.Get("current_directory")
 	if err != nil {
 		panic(err)
 	}
 
+	// Allowing file upload till 1GB file size.
+	r.ParseMultipartForm(1024 << 20)
 	file, header, err := r.FormFile("myFile")
 	if err != nil {
 		panic(err)
 	}
 	defer file.Close()
 
+	// Retrieving the file name
 	file_name := filepath.Base(header.Filename)
 
-	data, err := ioutil.ReadAll(file)
+	// Creating a new file, where we'll upload a new file
+	f, err := os.Create(fmt.Sprintf("%s/%s", current_directory, file_name))
 	if err != nil {
 		panic(err)
 	}
-
-	err = ioutil.WriteFile(fmt.Sprintf("%s/%s", current_directory, file_name), data, 0644)
+	defer f.Close()
+	
+	// Copying contents from the memory to the file we just created
+	_, err = io.Copy(f, file)
 	if err != nil {
 		panic(err)
 	}
