e635f89fce6a36dfbcb0466be5cb817d2c9cc4d1
Naru Koshin <naru.koshin@outlook.jp>
2023-02-04 05:44:32 +0200

    Improved a little bit code

--- app/handlers/UploadsHandler.go
+++ app/handlers/UploadsHandler.go
@@ -3,7 +3,6 @@ package handlers
 import (
 	"filemanager/app/sessions"
 	"filemanager/app/templates"
-	"fmt"
 	"io"
 
 	"net/http"
@@ -12,6 +11,7 @@ import (
 )
 
 func UploadsHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
 	// setting the template
 	template := templates.Template{}
 	template.FromFolder("upload").Import("upload.html")
@@ -34,19 +34,32 @@ func FileUploadHandler(w http.ResponseWriter, r *http.Request){
 
 	// Allowing file upload till 1GB file size.
 	r.ParseMultipartForm(1024 << 20)
-	file, header, err := r.FormFile("myFile")
+	file, _, err := r.FormFile("myFile")
 	if err != nil {
 		panic(err)
 	}
 	defer file.Close()
 
 	// Retrieving the file name
-	file_name := filepath.Base(header.Filename)
+	file_name := filepath.Base(r.FormValue("filename"))
 
-	// Creating a new file, where we'll upload a new file
-	f, err := os.Create(fmt.Sprintf("%s/%s", current_directory, file_name))
-	if err != nil {
-		panic(err)
+	// Building the file path
+	file_path := filepath.Join(current_directory, file_name)
+
+	// Checking if the file exists
+	var f *os.File
+	if _, err := os.Stat(file_path); os.IsNotExist(err){
+		// Creating a new file, where we'll upload a new file
+		f, err = os.Create(file_path)
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		// If the file already exists,writing to the file
+		f, err = os.OpenFile(file_path, os.O_WRONLY|os.O_APPEND, 0644)
+		if err != nil {
+			panic(err)
+		}
 	}
 	defer f.Close()
 	
@@ -54,5 +67,25 @@ func FileUploadHandler(w http.ResponseWriter, r *http.Request){
 	_, err = io.Copy(f, file)
 	if err != nil {
 		panic(err)
+	}
+
+	/******************** MEMORY LEAK SOLUTION ********************/
+	// Copying contents from the memory to the file we just created
+	// Add buffer to copy the contents to avoid memory leak
+	buf := make([]byte, 1024)
+	for {
+		// read a chunk
+		n, err := file.Read(buf)
+		if err != nil && err != io.EOF {
+			panic(err)
+		}
+		if n == 0 {
+			break
+		}
+
+		// write a chunk
+		if _, err := f.Write(buf[:n]); err != nil {
+			panic(err)
+		}
 	}
 }
