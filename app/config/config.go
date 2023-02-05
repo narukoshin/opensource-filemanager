f52e44cf92b6539b511dda93f9d3d1edbdf9983c
Naru Koshin <naru.koshin@outlook.jp>
2023-02-05 14:28:38 +0200

    Added logs folder and improved pathing

--- app/config/config.go
+++ app/config/config.go
@@ -1,6 +1,11 @@
 package config
 
+import "path/filepath"
+
 // Declaring some global variables
 var DefaultDirectory string = "uploads"
-var AssetsFolder 	 string = "app/assets"
-var TemplatesFolder  string	= "app/templates"
+
+var AppFolder		 string = "app"
+var AssetsFolder 	 string = filepath.Join(AppFolder, "assets")
+var TemplatesFolder  string	= filepath.Join(AppFolder, "templates")
+var LogsFolder		 string = filepath.Join(AppFolder, "logs")
