3b4f15452ace36aa1e5139b0fe77d2b99b4dd520
Naru Koshin <naru.koshin@outlook.jp>
2023-02-08 12:42:21 +0200

    Fixed an issue: #5

--- app/helpers/logger/logger.go
+++ app/helpers/logger/logger.go
@@ -17,24 +17,27 @@ var m *martini.ClassicMartini
 func Logger(){
 	m = routes.Martini
 
+	var err error
+	var f *os.File
+	
 	go func(){
 		for {
 			time.Sleep(time.Until(time.Now().Truncate(24 * time.Hour)))
 			// It will create a new file every day
 			file_name := fmt.Sprintf("logs_%s.log", time.Now().Format("2006_01_02"))
-			f, err := os.OpenFile(filepath.Join(config.LogsFolder, file_name), os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+			f, err = os.OpenFile(filepath.Join(config.LogsFolder, file_name), os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
 			if err != nil {
 				panic(err)
 			}
-
+			
 			m.Map(log.New(f, "[martini] ", log.LstdFlags))
-
+			
 			// add custom prints
 			m.Use(func(c martini.Context) {
 				log.SetPrefix("[martini] ")
 			})
-
 			log.SetOutput(f)
+			f.Close()
 		}
 	}()
 }
