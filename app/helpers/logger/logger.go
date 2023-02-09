9ec1e3394a6bb883afacee2072b620a79d7aedf0
Naru Koshin <naru.koshin@outlook.jp>
2023-02-09 08:40:02 +0200

    Added a better solution for #5

--- app/helpers/logger/logger.go
+++ app/helpers/logger/logger.go
@@ -14,24 +14,44 @@ import (
 
 var m *martini.ClassicMartini
 
-func Logger(){
+var logFile *os.File
+
+func CreateLogFile() {	
 	m = routes.Martini
+	// Create a new log file name for today
+	fileName := fmt.Sprintf("logs_%s.log", time.Now().Format("2006_01_02"))
 
-	go func(){
+	// Open the new log file
+	var err error
+	logFile, err = os.OpenFile(filepath.Join(config.LogsFolder, fileName), os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		log.Print(err)
+	}
+
+	// Set the log output to the new log file
+	m.Map(log.New(logFile, "[martini] ", log.LstdFlags))
+	m.Use(func(c martini.Context) {
+		log.SetPrefix("[martini] ")
+	})
+	log.SetOutput(logFile)
+	now := time.Now()
+	midnight := time.Date(now.Year(), now.Month(), (now.Day()+1), 0, 0, 0, 0, time.Local)
+	duration := midnight.Sub(now)
+	// Setting the prefix to logger
+	log.SetPrefix("[Logger] ")
+	log.Println("Sleeping for midnight: ", midnight)
+	log.Println("Time left: ", duration)
+	time.Sleep(duration)
+	// Closing the file after midnight
+	<-time.After(duration)
+	logFile.Close()
+}
+
+func Logger(){
+	// Create a new log file every day
+	go func() {
 		for {
-			time.Sleep(time.Until(time.Now().Truncate(24 * time.Hour)))
-			// It will create a new file every day
-			file_name := fmt.Sprintf("logs_%s.log", time.Now().Format("2006_01_02"))
-			f, _ := os.OpenFile(filepath.Join(config.LogsFolder, file_name), os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
-
-			m.Map(log.New(f, "[martini] ", log.LstdFlags))
-
-			// add custom prints
-			m.Use(func(c martini.Context) {
-				log.SetPrefix("[martini] ")
-			})
-
-			log.SetOutput(f)
+			CreateLogFile()
 		}
 	}()
 }
